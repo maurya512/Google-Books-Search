7f00b69d94481513feef27a9e0de915cad7f255d
Maurya Patel <patelmaurya0512@gmail.com>
2020-09-27 22:29:47 -0400

    Form component for react

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -2,7 +2,26 @@ import React from "react";
 
 function Form({ q, handleInputChange, handleFormSubmit }) {
   return (
-    // YOUR CODE HERE
+    <form>
+    <div className="form-row">
+      <div className="col-md-9">
+        <input
+          name="q"
+          list="book"
+          type="text"
+          onChange={handleInputChange}
+          className="form-control"
+          placeholder="Search Books"
+          id="book"
+        />
+      </div>
+      <div className="col-md-3">
+        <button className="btn btn-primary btn-sm form-control" type="submit" onClick={handleFormSubmit}>
+          Search
+        </button>
+      </div>
+    </div>
+  </form>
   );
 }
 

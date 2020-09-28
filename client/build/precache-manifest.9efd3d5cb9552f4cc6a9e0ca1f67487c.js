817efdd651c16d8c8fba3d124c5ab8eaf2c6f4ad
Maurya Patel <patelmaurya0512@gmail.com>
2020-09-27 22:28:31 -0400

    the page needed to run build

---
+++ client/build/precache-manifest.9efd3d5cb9552f4cc6a9e0ca1f67487c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "190342be214fcf864e66",
+    "url": "/static/css/main.494fadf4.chunk.css"
+  },
+  {
+    "revision": "190342be214fcf864e66",
+    "url": "/static/js/main.ce5ec906.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b51cceb99a46877f67dc",
+    "url": "/static/js/2.29356f6d.chunk.js"
+  },
+  {
+    "revision": "cb0ff482678d15e6d436c64740ad141c",
+    "url": "/index.html"
+  }
+];

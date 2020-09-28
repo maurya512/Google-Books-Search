cd12d2ddffc6ac42013a2a307a842b005a0d5912
Maurya Patel <patelmaurya0512@gmail.com>
2020-09-27 22:32:12 -0400

    fixed the queries with params and q to make the api work

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -3,26 +3,29 @@ const db = require("../models");
 // Defining methods for the bookController
 module.exports = {
   findAll: function(req, res) {
-    console.log("req.query", req.query);
     db.Book.find(req.query)
       .then(dbBook => res.json(dbBook))
       .catch(err => res.status(422).json(err));
   },
-
   findById: function(req, res) {
-    // YOUR CODE HERE
-
+    db.Book.findById(req.params.id)
+      .then(dbBook => res.json(dbBook))
+      .catch(err => res.status(422).json(err));
   },
   create: function(req, res) {
-    // YOUR CODE HERE
-    
+    db.Book.create(req.body)
+      .then(dbBook => res.json(dbBook))
+      .catch(err => res.status(422).json(err));
   },
   update: function(req, res) {
-    // YOUR CODE HERE
-    
+    db.Book.findOneAndUpdate({ id: req.params.id }, req.body)
+      .then(dbBook => res.json(dbBook))
+      .catch(err => res.status(422).json(err));
   },
   remove: function(req, res) {
-    // YOUR CODE HERE
-    
+    db.Book.findById(req.params.id)
+      .then(dbBook => dbBook.remove())
+      .then(dbBook => res.json(dbBook))
+      .catch(err => res.status(422).json(err));
   }
 };
